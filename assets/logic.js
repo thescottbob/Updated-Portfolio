64413c7dd3905174d34956cc85341169ffc15311
thescottbob <scottmcmains@gmail.com>
2018-12-19 21:22:24 -0800

    Squashed some bugs

--- assets/logic.js
+++ assets/logic.js
@@ -1,54 +1,57 @@
 window.onload = function() {
-    $("#portfolioPage").hide();
-    $("#aboutMePage").hide();
-    $("#contactMePage").hide();
-  };
+  $(".page").hide();
+  $("#homePage").show();
+};
 
-$(document).ready(function(){
+$(document).ready(function() {
+  //On click events if any of the buttons are clicked
+  $("#portfolio").click(function() {
+    $("#homePage").fadeOut("slow");
+    $("#portfolioPage").fadeIn("slow");
+  });
 
-    //On click events if any of the buttons are clicked
-    $("#portfolio").click(function(){
-        $("#homePage").fadeOut("slow");
-        $("#portfolioPage").fadeIn("slow");
-    });
+  $("#aboutMe").click(function() {
+    $("#homePage").fadeOut("slow");
+    $(".aboutMePage").fadeIn("slow");
+  });
 
-    $("#aboutMe").click(function(){
-        $("#homePage").fadeOut("slow");
-        $("#aboutMePage").fadeIn("slow");
-    });
+  $("#contactMe").click(function() {
+    $("#homePage").fadeOut("slow");
+    $("#contactMePage").fadeIn("slow");
+  });
 
-    $("#contactMe").click(function(){
-        $("#homePage").fadeOut("slow");
-        $("#contactMePage").fadeIn("slow");
-    });
+  $("#github").click(function() {
+    $("#githubLink").attr("target", "_blank");
+    window.open("https://github.com/thescottbob");
+  });
 
-    $("#github").click(function(){
-        $("#githubLink").attr("target", "_blank");
-        window.open("https://github.com/thescottbob");
-    });
+  $("#linkedIn").click(function() {
+    $("#linkedinLink").attr("target", "_blank");
+    window.open("https://www.linkedin.com/in/scottmcmains/");
+  });
 
-    $("#linkedIn").click(function(){
-        $("#linkedinLink").attr("target", "_blank");
-        window.open("https://www.linkedin.com/in/scottmcmains/");
-    });
+  $("#aboutMeHomeButton").click(function() {
+    $(".aboutMePage").fadeOut("slow");
+    $("#aboutMeHomeButton").fadeOut("slow");
+    $("#homePage").fadeIn("slow");
+  });
 
-    $("#aboutMeHomeButton").click(function(){
-        $("#aboutMePage").fadeOut("slow");
-        $("#portfolioPage").fadeOut("slow");
-        $("#homePage").fadeIn("slow");
-    })
+  // When any of the images on the Portfolio page are clicked on
+  $("#foodeaze").click(function() {
+    window
+      .open("https://github.com/thescottbob/FoodEaze")
+      .attr("target", "_blank");
+  });
 
-    // When any of the images on the Portfolio page are clicked
-    $("#foodeaze").click(function(){
-        window.open("https://github.com/thescottbob/FoodEaze").attr("target", "_blank");
-    })
+  $("#enviral").click(function() {
+    window
+      .open("https://github.com/kyleconyers/Enviral")
+      .attr("target", "_blank");
+  });
 
-    $("#enviral").click(function(){
-        window.open("https://github.com/kyleconyers/Enviral").attr("target", "_blank");
-    })
-
-    $("#bamazon").click(function(){
-        window.open("https://github.com/thescottbob/Bamazon").attr("target", "_blank");
-    })
-
+  $("#bamazon").click(function() {
+    window
+      .open("https://github.com/thescottbob/Bamazon")
+      .attr("target", "_blank");
+  });
 });
